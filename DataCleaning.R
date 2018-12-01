8fe13e6ebdce3ef21d18b40c701c8a5667be63f7
Nanshan Li <nanshan7596@gmail.com>
2018-12-01 12:38:53 -0500

    Added Bangladesh

--- DataCleaning.R
+++ DataCleaning.R
@@ -17,7 +17,7 @@ CrisesOriginRefugees <- Refugees %>%
 write.csv(CrisesOriginRefugees,'CrisesOrigin.csv')
 
 # Where refugees are going now
-LDRefugeeCountries <- c("Turkey", "Jordan", "Pakistan", "Lebanon","Iran", "Ethiopia", "Sudan", "Kenya", "Dem. Rep. of the Congo", "Chad")
+LDRefugeeCountries <- c("Turkey", "Jordan", "Pakistan", "Lebanon","Iran", "Ethiopia", "Sudan", "Kenya", "Dem. Rep. of the Congo", "Chad", "Bangladesh")
 MDRefugeeCountries <- c("Germany", "Sweden")
 LargestPoplnInWorld <- c("India", "China", "United States of America") 
 MediterranianCountries <- c("Greece", "Italy")
